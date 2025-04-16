e9e84b5213f5df553865df7255a6bcdec794c82c
simonedambrogio <simonedambrogio@live.it>
2025-04-16 19:50:59 +0100

    initialize _core networks

--- dreamerv3/rssm.jl
+++ dreamerv3/rssm.jl
@@ -6,22 +6,100 @@ include("../embodied/lux/ReArrange.jl");
 # Note: We might need more includes later as we add specific layers
 
 # Based on Python RSSM class attributes
-@kwdef struct RSSM <: Lux.AbstractLuxLayer # Using @kwdef for easier construction
-    deter_dim::Int = 4096
-    hidden_dim::Int = 2048
-    stoch_dim::Int = 32
-    classes_dim::Int = 32
-    act::Function = gelu
-    unroll::Bool = false
-    unimix::Float32 = 0.01f0
-    imglayers::Int = 2
-    obslayers::Int = 1
-    dynlayers::Int = 1
-    blocks::Int = 8
-    free_nats::Float32 = 1.0f0
-    act_space::Space
-    # Sub-layers will be added here later
+struct RSSM{AS, CN} <: Lux.AbstractLuxContainerLayer{(:core,)} # Added AS for type stability
+    deter_dim::Int
+    hidden_dim::Int
+    stoch_dim::Int
+    classes_dim::Int
+    act::Function
+    unroll::Bool
+    unimix::Float32
+    imglayers::Int
+    obslayers::Int
+    dynlayers::Int
+    blocks::Int
+    free_nats::Float32
+    act_space::AS # Use type parameter AS
+    # Sub-layers defined in core
+    core::CN
 end
+
+function RSSM(; # Constructor
+    deter_dim::Int = 4096,
+    hidden_dim::Int = 2048,
+    stoch_dim::Int = 32,
+    classes_dim::Int = 32,
+    act::Function = gelu,
+    unroll::Bool = false,
+    unimix::Float32 = 0.01f0,
+    imglayers::Int = 2,
+    obslayers::Int = 1,
+    dynlayers::Int = 1,
+    blocks::Int = 8,
+    free_nats::Float32 = 1.0f0,
+    act_space::Space) # act_space is mandatory
+
+    # --- Calculate Static Dimensions ---
+    g = blocks
+    @assert deter_dim % g == 0 "deter_dim must be divisible by blocks (g)"
+    @assert (stoch_dim * classes_dim) % g == 0 "stoch_dim*classes_dim must be divisible by blocks (g)" # Might need this if stoch is used in BlockLinear
+    @assert (3 * deter_dim) % g == 0 "3*deter_dim must be divisible by blocks (g)" # For gru_layer output
+    num_actions = act_space.high + 1 # Assuming discrete Space
+
+    # --- Define Core Layers --- 
+
+    # Layers for initial context processing (deter, stoch, action)
+    layer_deter = Chain(
+        Dense(deter_dim => hidden_dim; init_weight=cast_glorot_uniform, init_bias=cast_zeros),
+        RMSNorm((hidden_dim,), act; dims=(1,), init_scale=cast_ones)
+    )
+    layer_stoch = Chain(
+        Dense(stoch_dim * classes_dim => hidden_dim; init_weight=cast_glorot_uniform, init_bias=cast_zeros),
+        RMSNorm((hidden_dim,), act; dims=(1,), init_scale=cast_ones)
+    )
+    layer_action = Chain(
+        Dense(num_actions => hidden_dim; init_weight=cast_glorot_uniform, init_bias=cast_zeros),
+        RMSNorm((hidden_dim,), act; dims=(1,), init_scale=cast_ones)
+    )
+
+    # Dynamic layers loop (dynhid + dynhidnorm)
+    dyn_layers_list = []
+    # Calculate input dim for the *first* dynamic layer
+    h_deter_per_block = deter_dim ÷ g
+    feat_concat_static = 3 * hidden_dim
+    first_dyn_input_dim = (h_deter_per_block + feat_concat_static) * g
+    current_dyn_input_dim = first_dyn_input_dim
+
+    for _ in 1:dynlayers
+        push!(dyn_layers_list, Chain(
+            BlockLinear(current_dyn_input_dim, deter_dim, g; init_weight=cast_glorot_uniform, init_bias=cast_zeros),
+            RMSNorm((deter_dim,), act; dims=(1,), init_scale=cast_ones)
+        ))
+        current_dyn_input_dim = deter_dim # Input for subsequent layers is the output of the previous one
+    end
+    dyn_layers = Chain(dyn_layers_list...; name="dyn_layers_loop")
+
+    # Final GRU layer (dyngru)
+    gru_layer_input_dim = deter_dim # Output of the dyn_layers loop
+    gru_layer_output_dim = 3 * deter_dim
+    gru_layer = BlockLinear(gru_layer_input_dim, gru_layer_output_dim, g; init_weight=cast_glorot_uniform, init_bias=cast_zeros)
+
+    # --- Store Layers in NamedTuple --- 
+    core_layers = (
+        layer_deter = layer_deter,
+        layer_stoch = layer_stoch,
+        layer_action = layer_action,
+        dyn_layers = dyn_layers, # This is now a Chain
+        gru_layer = gru_layer
+    )
+
+    # --- Return RSSM Instance --- 
+    # Automatically determine types AS and CN
+    return RSSM(deter_dim, hidden_dim, stoch_dim, classes_dim, act, unroll, unimix, 
+                imglayers, obslayers, dynlayers, blocks, free_nats, 
+                act_space, core_layers)
+end
+
 
 """
     initial_state(rssm::RSSM, batch_size::Int, ::AbstractRNG)
@@ -30,12 +108,9 @@ Returns the initial recurrent state for the RSSM.
 Output shape: (deter = (batch_size, deter_dim), stoch = (batch_size, stoch_dim, classes_dim))
 """
 function LuxCore.initialstates(rng::AbstractRNG, rssm::RSSM)
-    # This function is designed to be called via Lux.setup, which doesn't provide batch_size.
-    # The actual state initialization with batch_size needs a separate function
-    # or handling within the main call method when the first batch arrives.
-    # For now, returning an empty state for setup purposes.
-    # We'll define a separate function for runtime initialization.
-    return NamedTuple() # LuxCore.initialstates expects states of sublayers
+    # Delegate state initialization to the sub-layers stored in rssm.core
+    # This will recursively call initialstates on the layers within the core NamedTuple.
+    return (; core = Lux.initialstates(rng, rssm.core))
 end
 
 """
@@ -108,9 +183,19 @@ function _observe(rssm::RSSM, carry::NamedTuple, tokens, action::AbstractVector{
     # return carry_next, (entry, feat)
 end
 
-function _core(rssm::RSSM, deter::AbstractArray, stoch::AbstractArray, action::AbstractArray)
+rng = Random.default_rng()
+ps, st = Lux.setup(rng, rssm)
+
+function _core(rssm::RSSM, deter::AbstractArray, stoch::AbstractArray, action::AbstractArray, ps, st)
     # Combine classes and stoch
-    stoch = reshape(stoch, (:, size(stoch,3)))  #
+    stoch = reshape(stoch, (:, size(stoch,3)))  # Shape: (stoch_dim * classes_dim, Batch)
+    
+    # Apply dynamic layers
+    deter, st.core.layer_deter = rssm.core.layer_deter(deter, ps.core.layer_deter, st.core.layer_deter)
+    stoch, st.core.layer_stoch = rssm.core.layer_stoch(stoch, ps.core.layer_stoch, st.core.layer_stoch)
+    action, st.core.layer_action = rssm.core.layer_action(action, ps.core.layer_action, st.core.layer_action)
+
+    
 end
 
 # TODO: Define sub-layers and constructor
